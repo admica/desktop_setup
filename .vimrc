65822fa1e1f414b8a53ac5456909fdd3a92b5c18
The Name <nobody@noemail.sorry>
2025-03-19 13:12:12 -0500

    added installer and .vimrc

---
+++ .vimrc
@@ -0,0 +1,46 @@
+" Enable syntax highlighting
+syntax on
+
+" Set the encoding to UTF-8
+set encoding=utf-8
+
+" Set the file encoding to UTF-8
+set fileencoding=utf-8
+
+" Show line numbers
+set number
+
+" Highlight all instances of searched text
+set hlsearch
+
+" Ignore case when searching
+set ignorecase
+
+" Make search case-insensitive and highlight matches
+set incsearch
+
+" Use 4 spaces instead of tabs
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+" Enable auto indentation
+set autoindent
+
+" Enable smart indentation
+set smartindent
+
+" Set the ruler to show the current line and column
+set ruler
+
+" Show the current command in the status line
+set showcmd
+
+" Set the color scheme to desert
+colorscheme torte
+
+" Map Ctrl + S to save the file
+nnoremap <C-s> :w<CR>
+
+" Map Ctrl + Q to quit the file
+nnoremap <C-q> :q<CR>
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# PATH: ./install.sh
+# Installation Script
+
+# Exit immediately on error
+set -e
+
+# Define color codes for better output readability
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+print_header() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    local message="[${timestamp}] ### $1 ###"
+    echo -e "${GREEN}${message}${NC}"
+    echo "$message" >> "$LOGFILE"
+}
+
+print_action() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    local message="[${timestamp}] >>> $1"
+    echo -e "${BLUE}${message}${NC}"
+    echo "$message" >> "$LOGFILE"
+}
+
+print_error() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    local message="[${timestamp}] ERROR: $1"
+    echo -e "${RED}${message}${NC}"
+    echo "$message" >> "$LOGFILE"
+}
+
+print_detail() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    local message="[${timestamp}]     $1"
+    echo -e "${CYAN}${message}${NC}"
+    echo "$message" >> "$LOGFILE"
+}
+
+print_header "Copying Files"
+print_detail "copy .bash_prompt"
+cp -f .bash_prompt ~/
+
+print_header "Rsync Dirs"
+print_detail "rsync .local -> ~/"
+rsync -avzh ./.local/ ~/.local/
+
+print_detail "rsync .config -> ~/"
+rsync -avzh ./.local/ ~/.config/
+
+print_header "update font cache"
+fc-cache -fv
+
+print_header "Check .bashrc"
+grep "bash_prompt" ~/.bashrc 2>/dev/null
+
+if [[ $? -ne 0 ]]; then
+    print_detail "adding source ~/.bash_prompt to ~/.bashrc"
+    echo "source ~/.bash_prompt" >> ~/.bashrc
+else
+    print_detail "bash_prompt already called from .bashrc"
+fi
+
+print_header "Installation Complete!"
